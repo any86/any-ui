32ee0158b54cfae73a22ab4a59993eb7121e1508
宁宁 <383514580@qq.com>
2017-05-11 17:11:16 +0800

    制作sidebar组件

---
+++ src/packages/SideBar/Sidebar.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="component-side-bar">
+        <Modal v-model="isShow"></Modal>
+        <transition name="side">
+            <div v-show="isShow" class="side-bar">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+<script>
+import Modal from '../Dialog/Modal';
+
+export default {
+    name: 'SideBar',
+
+    props: {
+        value: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    methods: {
+        closeModal() {
+            this.$emit('update:isShowSideBar', false);
+        }
+    },
+
+    computed: {
+        isShow: {
+            get() {
+                return this.value;
+            },
+
+            set(value) {
+                this.$emit('input', value);
+            }
+        }
+    },
+
+    components: {
+        Modal
+    }
+}
+</script>
+<style scoped lang=scss>
+@import '../../scss/theme.scss';
+.side-bar {
+    box-shadow: $shadowDown;
+    z-index: $sideBarZIndex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 80%;
+    background: $background;
+    transform: translateX(0);
+}
+
+.side-enter-active {
+    animation: side-in .5s;
+}
+
+.side-leave-active {
+    animation: side-out .5s;
+}
+
+@keyframes side-in {
+    0% {
+        transform: translateX(-100%);
+    }
+    100% {
+        transform: translateX(0);
+    }
+}
+
+@keyframes side-out {
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(-100%);
+    }
+}
+</style>
