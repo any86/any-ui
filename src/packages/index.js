af1f7370f3f0fac791b183c91be287394a039003
宁宁 <383514580@qq.com>
2017-05-11 17:08:54 +0800

    支持引入alert/confirm等对话框组件插件化

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,4 +1,7 @@
+import DialogServer from './Dialog/DialogServer'
+
 import Icon from './Icon/Icon';
+import Spinner from './Spinner/Spinner';
 import ScrollView from './ScrollView/ScrollView';
 import Flexbox from './Flexbox/FlexBox';
 import FlexItem from './Flexbox/FlexItem';
@@ -10,5 +13,62 @@ Atom.install = function(Vue) {
     Vue.component(FlexItem.name, FlexItem);
     Vue.component(ScrollView.name, ScrollView);
     Vue.component(Icon.name, Icon);
+    Vue.component(Spinner.name, Spinner);
+
+    // Vue.component('VPrompt', Prompt);
+    // document.createDocumentFragment()
+    var DialogServerComponent = Vue.extend(DialogServer);
+    // 创建一个挂载点
+    var node = document.createElement('div');
+    // 起个不重复的名字
+    node.id = '_app-dialog-' + Math.ceil(Math.random());
+    document.body.appendChild(node);
+    // 挂载
+    var vm = new DialogServerComponent().$mount('#' + node.id);
+    // =================================================
+    // ==============组件内调用: this.$alert==============
+    // =================================================
+    Vue.prototype.$alert = (text = '', options = {}) => {
+        return new Promise((resolve, reject) => {
+            vm.modal.show = true;
+            vm.alert = {
+                ...vm.alert,
+                ...options,
+                ok: () => {
+                    vm.modal.show = false;
+                    resolve();
+                },
+                show: true,
+                text
+
+            };
+        });
+    };
+    // =================================================
+    // ==============组件内调用: this.$confirm============
+    // =================================================
+    Vue.prototype.$confirm = (text = '', options = {}) => {
+        return new Promise((resolve, reject) => {
+            vm.modal.show = true;
+            vm.confirm = {
+                ...vm.alert,
+                ...options,
+                ok: () => {
+                    vm.modal.show = false;
+                    resolve();
+                },
+                cancel: () => {
+                    vm.modal.show = false;
+                    reject();
+                },
+                show: true,
+                text
+            };
+
+        });
+    };
+
+
+
 };
 export default Atom;
