3e45a669b7849c820d33aa10fe4a9ad99c7ac054
宁宁 <383514580@qq.com>
2017-05-11 17:09:56 +0800

    制作拖动列表组件

---
+++ src/packages/List/ListItem.vue
@@ -0,0 +1,112 @@
+<template>
+    <li class="component-list-item">
+        <div :class="{body:true, transition: 2 != touche.status}" :style="{transform: 'translateX('+ touche.distance +'px)'}">
+            <div class="content-bar" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
+                <slot></slot>
+            </div>
+            <div ref="actionBar" class="action-bar" > 
+                <!-- <a class="button-edit">更多</a> -->
+                <a @click="remove" class="button-del">删除</a>
+            </div>
+        </div>
+    </li>
+</template>
+<script>
+export default {
+    name: 'ListItem',
+
+    props: {
+        actionShow: {
+            type: Boolean,
+            default: false
+        },
+
+        value: {}
+    },
+
+    data() {
+        return {
+            actionBar: {
+                width: 0
+            },
+            touche: {
+                status: 0,
+                start: 0,
+                current: 0,
+                distance: 0
+            }
+        };
+    },
+
+    mounted() {
+        this.actionBar.width = this.$refs.actionBar.offsetWidth;
+    },
+
+    methods: {
+        remove(){
+            this.$confirm('123').then(()=>{}).catch(()=>{});
+        },
+
+        touchStart(e) {
+            this.touche.distance = 0;
+            this.touche.status = 1;
+            this.touche.start = e.touches[0].clientX;
+        },
+
+        touchMove(e) {
+            this.touche.status = 2;
+            this.touche.current = e.touches[0].clientX;
+            var distance = (this.touche.current - this.touche.start);
+            // 只能从左向右拖拽
+            // 并且距离不能大于hidden-bar宽度
+            if (0 > distance && 0 - distance <= this.actionBar.width) {
+                this.touche.distance = this.touche.current - this.touche.start;
+            }
+        },
+
+        touchEnd(e) {
+            if(0 - this.touche.distance > this.actionBar.width / 2) {
+                this.touche.distance = 0 - this.actionBar.width;
+            } else {
+                this.touche.distance = 0;
+            }
+            this.touche.status = 0;
+        }
+    },
+
+    components: {}
+}
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.component-list-item {
+    position: relative;
+    overflow: hidden;
+    border-bottom: 1px solid $lightest;
+    >.body {
+        box-sizing: border-box;
+        position: relative;
+        width: 100%;
+
+        >.content-bar {
+            box-sizing: border-box;
+            position: relative;
+            width: 100%;
+        }
+        >.action-bar {
+            transform:translateX(100%);
+            position: absolute;
+            top: 0;right:0;
+            z-index: 2;
+            height: 100%;
+            display: flex;
+            .button-edit{position: relative;background: $default;color: #fff;height: 100%;display: flex; align-items: center;width: 100%;padding:0 3*$gutter;font-size: 14px;letter-spacing: 1px;}
+            .button-del{position: relative;background: $danger;color: #fff;height: 100%;display: flex; align-items: center;width: 100%;padding:0 3*$gutter;font-size: 14px;letter-spacing: 1px;}
+        }
+    }
+}
+
+.transition {
+    transition: all .2s ease;
+}
+</style>
---
+++ src/packages/List/ListView.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="component-list">
+        <slot></slot>
+    </ul>
+</template>
+<script>
+export default {
+    name: 'List',
+
+    props: {
+        value: {}
+    },
+
+    mounted() {},
+
+    data() {
+        return {}
+    },
+
+    methods: {},
+
+    components: {}
+}
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.component-list {
+   position: relative;
+   li:last-child{border-bottom: none;}
+}
+</style>
