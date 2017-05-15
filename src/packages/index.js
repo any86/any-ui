08d49f43a81e9cf52e0f3cd71aa9a96f1b66850e
宁宁 <383514580@qq.com>
2017-05-15 16:50:18 +0800

    更改DialogServer变量名为DialogRoot

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,4 +1,4 @@
-import DialogServer from './Dialog/DialogServer'
+import DialogRoot from './Dialog/DialogRoot'
 
 import Icon from './Icon/Icon';
 import Spinner from './Spinner/Spinner';
@@ -15,31 +15,29 @@ Atom.install = function(Vue) {
     Vue.component(Icon.name, Icon);
     Vue.component(Spinner.name, Spinner);
     Vue.component(Badge.name, Badge);
-    
-
 
 
     // Vue.component('VPrompt', Prompt);
     // document.createDocumentFragment()
-    var DialogServerComponent = Vue.extend(DialogServer);
+    var DialogRootComponent = Vue.extend(DialogRoot);
     // 创建一个挂载点
     var node = document.createElement('div');
     // 起个不重复的名字
     node.id = '_app-dialog-' + Math.ceil(Math.random());
     document.body.appendChild(node);
     // 挂载
-    var vm = new DialogServerComponent().$mount('#' + node.id);
+    var vm = new DialogRootComponent().$mount('#' + node.id);
     // =================================================
     // ==============组件内调用: this.$alert==============
     // =================================================
     Vue.prototype.$alert = (text = '', options = {}) => {
         return new Promise((resolve, reject) => {
-            vm.modal.show = true;
+            vm.mask.show = true;
             vm.alert = {
                 ...vm.alert,
                 ...options,
                 ok: () => {
-                    vm.modal.show = false;
+                    vm.mask.show = false;
                     resolve();
                 },
                 show: true,
@@ -53,16 +51,16 @@ Atom.install = function(Vue) {
     // =================================================
     Vue.prototype.$confirm = (text = '', options = {}) => {
         return new Promise((resolve, reject) => {
-            vm.modal.show = true;
+            vm.mask.show = true;
             vm.confirm = {
                 ...vm.alert,
                 ...options,
                 ok: () => {
-                    vm.modal.show = false;
+                    vm.mask.show = false;
                     resolve();
                 },
                 cancel: () => {
-                    vm.modal.show = false;
+                    vm.mask.show = false;
                     reject();
                 },
                 show: true,
