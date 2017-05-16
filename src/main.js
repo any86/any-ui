f988529ab81085d75320506f12adcb70b8e12ff5
宁宁 <383514580@qq.com>
2017-05-16 17:17:59 +0800

    从写了syslog逻辑

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-window.syslog = (val) => console.log(val);
+window.syslog = (...val) => console.log.apply(null, val);
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
