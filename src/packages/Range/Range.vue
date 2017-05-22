3e26b6502363f815fe312ff24b18149e2faa56ce
宁宁 <383514580@qq.com>
2017-05-22 17:22:26 +0800

    制作range组件

---
+++ src/packages/Range/Range.vue
@@ -0,0 +1,181 @@
+<template>
+    <div class="component-range">
+        <span class="min">{{min}}</span>
+        <span class="content">
+            <div ref="runway" class="runway"></div>
+            <div :style="{width: touch.translateXNew + 'px'}" :class="['progress', 0 == touch.status && 'transition']"></div>
+            <div ref="handle" :style="{transform: 'translate3d(' + touch.translateXNew + 'px, 0, 0)'}" :class="['handle', 0 == touch.status && 'transition']" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend"></div>
+        </span>
+        <span class="max">{{max}}</span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Range',
+
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+
+        min: {
+            type: Number,
+            default: 0
+        },
+
+        max: {
+            type: Number,
+            default: 100
+        },
+
+        step: {
+            type: Number
+        },
+
+        value: {
+            type: [Number, String],
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            maxDistance: 0, // 滑道可用宽度
+            touch: {
+                translateXOld: 0,
+                translateXNew: 0,
+                status: 0,
+                start: 0,
+                current: 0,
+                distance: 0
+            }
+        };
+    },
+
+    mounted() {
+        // 滑道宽度 - 把手宽度
+        this.maxDistance = this.$refs.runway.offsetWidth - this.$refs.handle.offsetWidth;
+        // 滑动把手到默认值位置
+        this._default();
+    },
+
+    methods: {
+        /**
+         * 滑动把手到默认值位置
+         */
+        _default() {
+            this.touch.translateXNew = (this.value - this.min) / (this.max - this.min) * this.maxDistance;
+            this.touch.translateXOld = this.touch.translateXNew;
+        },
+
+        touchstart(e) {
+            this.touch.status = 1;
+            this.touch.start = e.touches[0].clientX;
+
+        },
+
+        touchmove(e) {
+            this.touch.status = 2;
+            this.touch.current = e.touches[0].clientX;
+            this.touch.distance = this.touch.current - this.touch.start;
+
+            // ** 判断放在最外层有不同的动画效果 **
+            // 判断边界
+            var translateXNew = this.touch.translateXOld + this.touch.distance;
+            if (0 > translateXNew) {
+                // 左侧边界判断
+                this.touch.translateXNew = 0;
+            } else if (this.maxDistance < translateXNew) {
+                // 右侧边界判断
+                this.touch.translateXNew = this.maxDistance;
+            } else {
+                this.touch.translateXNew = translateXNew;
+            }
+        },
+
+        touchend(e) {
+            this.touch.status = 0;
+            if (0 > this.touch.translateXNew) {
+                this.touch.translateXNew = 0;
+            }
+
+            // 更新滑动位置
+            this.touch.translateXOld = this.touch.translateXNew;
+
+            // 同步value
+            var value = this.min + (this.max - this.min) * (this.touch.translateXNew / this.maxDistance);
+            this.$emit('input', Math.round(value));
+        }
+    },
+
+    watch: {
+        value() {
+            // 滑动把手到默认值位置
+            this._default();
+        }
+    }
+}
+</script>
+<style scoped lang=scss>
+@import '../../scss/theme.scss';
+$height: 30px;
+.component-range {
+    position: relative;
+    display: flex;
+    .min {
+        padding: 0 $gutter;
+        font-size: $big;
+        line-height: $height;
+        flex: 1;
+    }
+    .content {
+        position: relative;
+        height: $height;
+        width: 100%;
+        padding: 0 $gutter;
+        box-sizing: border-box;
+        .runway {
+            position: absolute;
+            top: 50%;
+            left: 0;
+            transform: translateY(-50%);
+            background: $light;
+            height: 1px;
+            width: 100%;
+        }
+        .progress {
+            position: absolute;
+            top: 50%;
+            left: 0;
+            transform: translateY(-50%);
+            background: $base;
+            height: 1px;
+            width: 0;
+        }
+        .handle {
+            position: absolute;
+            z-index: 2;
+            top: 0;
+            left: 0;
+            cursor: move;
+            display: block;
+            height: $height;
+            width: $height;
+            border-radius: 100%;
+            background: #fff;
+            box-shadow: $shadowDown, $shadowUp;
+        }
+    }
+    .max {
+        padding: 0 $gutter;
+        font-size: $big;
+        line-height: $height;
+        flex: 1;
+    }
+}
+
+.transition {
+    transition: all .3s;
+}
+</style>
