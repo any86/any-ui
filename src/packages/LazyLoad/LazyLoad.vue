e16fe9434182d1eb990896d23bce1ecc787c4fd5
宁宁 <383514580@qq.com>
2017-06-05 10:16:40 +0800

    仅仅实现了监听指定元素的scroll的逻辑

--- src/packages/LazyLoad/LazyLoad.vue
+++ src/packages/LazyLoad/LazyLoad.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="component-lazyload" :lazy="statusString" :style="{width, height}">
-        <img v-if="0 == status || !isLoad" src="./loading.gif">
+        <img v-if="0 == status" src="./loading.gif">
         <transition name="alert">
-            <img v-if="1 == status && isLoad" :src="src">
+            <img v-if="1 == status" :src="src">
         </transition>
     </div>
 </template>
@@ -11,30 +11,23 @@ export default {
     name: 'LazyLoad',
 
     props: {
+        element: {
+            required: true
+        },
+
+        event: {
+            type: String,
+            default: 'scroll'
+        },
+
         src: {
             type: String,
         },
 
-        isLoad: {
-            type: Boolean
+        attempt: {
+            type: Number,
+            default: 1
         }
-    },
-
-    mounted() {
-
-        this.attempt = this.$el.getAttribute('attempt');
-        this.height = this.$el.getAttribute('height');
-        this.width = this.$el.getAttribute('width');
-
-        this.loadImg();
-
-        // 等待style生效, 组件尺寸生效占位后才能获取到实际top
-        this.$nextTick(() => {
-            var obj  = this.$el.getBoundingClientRect();
-            this.top = obj.top;
-            this.bottom = obj.bottom;
-            this.height = obj.height + 'px';
-        });
     },
 
     data() {
@@ -45,17 +38,31 @@ export default {
             width: '0',
             height: '0',
             loadTime: 0,
-            attempt: 3,
             placeholder: ''
         };
     },
 
+    mounted() {
+
+        this.height = this.$el.getAttribute('height');
+        this.width = this.$el.getAttribute('width');
+
+        // 等待style生效, 组件尺寸生效占位后才能获取到实际top
+        this.$nextTick(() => {
+            this.loadImg();
+            this.element.addEventListener(this.event, this.loadImg /*, {once: true}*/ );
+        });
+    },
+
     methods: {
         loadImg() {
+            var obj = this.$el.getBoundingClientRect();
+            this.top = obj.top;
+            this.bottom = obj.bottom;
+            this.height = obj.height + 'px';
 
-            // if(img.compulet);
-
-            if (this.isLoad) {
+            if (this.top < window.screen.height) {
+                // if(img.compulet);
                 // const DEFAULT_URL = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
                 // const DEFAULT_EVENTS = ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend', 'touchmove']
                 let img = new Image();
@@ -73,15 +80,17 @@ export default {
                     this.status = -1;
                     syslog(e);
                 }
+
+                this.element.removeEventListener(this.event, this.loadImg);
             }
         }
     },
 
     computed: {
-        isInView(){
+        isInView() {
             // return window.screen.height;
             return window.screen.height;
-            
+
         },
 
         statusString() {
@@ -96,12 +105,6 @@ export default {
                     return 'done';
                     break;
             }
-        }
-    },
-
-    watch: {
-        isLoad() {
-            this.loadImg();
         }
     }
 }
