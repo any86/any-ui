00882aac8e7df2afe47954d462b091d3ce4ce800
宁宁 <383514580@qq.com>
2017-06-05 17:24:00 +0800

    制作圆形进度条

---
+++ src/packages/Progress/Circle.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="component-circle">
+        <p>{{value}}%</p>
+        <svg :viewBox="`0 0 100 100`" xmlns="http://www.w3.org/2000/svg">
+            <circle class="backdrop" :r="40" :cy="50" :cx="50" stroke-width="3" stroke="#ddd" fill="none" />
+            <circle class="progress" :r="40" :cy="50" :cx="50" stroke-width="3" stroke="none" fill="none" :stroke-dasharray="strokeDasharray" />
+        </svg>
+    </div>
+</template>
+<script>
+// 假定viewBox为100 * 100
+export default {
+    name: 'Circle',
+
+    props: {
+        value: {
+            type: Number,
+            default: 80
+
+        },
+    },
+
+    computed: {
+        strokeDasharray() {
+            return `${2 * Math.PI * 40 * this.value / 100} ${2 * Math.PI * 40}`;
+        }
+    }
+
+}
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.component-circle {
+    position: relative;
+    overflow: hidden;
+    p{font-size: $biggest;margin: auto;text-align: center;position: absolute;z-index: 2;width: 100%;transform:translateY(-50%);top:50%;color:$base;}
+
+    svg {
+        transform: rotate(-90deg);
+        >.backdrop {
+            stroke: $lighter;
+        }
+        >.progress {
+            stroke: $base;
+            transition:stroke-dasharray 1s;
+        }
+       
+        >text {
+            fill: #f10;
+        }
+    }
+}
+</style>
