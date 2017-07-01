ab6431bde7c1e719e36a262edacffabddea9272c
宁宁 <383514580@qq.com>
2017-07-01 17:27:00 +0800

    mock 地区api

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,15 @@
-import axios from "axios";
-const getGoodsList = params => axios.get("./mock/imgs", { params });
+import axios from 'axios';
 
-const getBagPage = params => axios.get("./static/bag.json", { params });
+const getGoodsList = params => axios.get('./mock/imgs', { params });
 
-const getZoneLevel1 = params => axios.get("./mock/zone1", { params });
-const getZoneLevel2 = params => axios.get("./mock/zone2", { params });
-const getZoneLevel3 = params => axios.get("./mock/zone3", { params });
+const getBagPage = params => axios.get('./static/bag.json', { params });
 
-const getZone = (level, params) => axios.get("./mock/zone" + (parseInt(level) + 1), { params });
+const getZone = (level, params) => axios.get('./mock/zone' + (parseInt(level)), { params });
 
-
+const saveAddress = params => axios.post('./mock/success', params);
 
 export default {
     getGoodsList,
     getBagPage,
-    getZoneLevel1,
-    getZoneLevel2,
-    getZoneLevel3,getZone,
+    getZone,saveAddress
 };
