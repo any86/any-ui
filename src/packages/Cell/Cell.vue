f55715dce0320cb26a4629a0d918935543a35a01
宁宁 <383514580@qq.com>
2017-07-07 12:06:09 +0800

    修改样式

--- src/packages/Cell/Cell.vue
+++ src/packages/Cell/Cell.vue
@@ -20,20 +20,19 @@ export default {
 @import '../../scss/theme.scss';
 .component-cell {
     display: flex;
-        padding: 0 2*$gutter;
-        min-height: 1.2rem;
-
+    background: $background;
+    padding: 0 2*$gutter;
+    min-height: 1.2rem;
     >span {
         flex: 1;
+        position: relative;
         align-self: center;
         font-size: $big;
         color: $dark;
-
     }
     >.icon {
         align-self: center;
         font-size: $biggest;
-        
     }
 }
 </style>
