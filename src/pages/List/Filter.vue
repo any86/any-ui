d584428d506536cf89d9ad06c84d50c7f9fc0506
宁宁 <383514580@qq.com>
2017-08-03 17:23:11 +0800

    移除滑动自动吸顶 增加点击分类自动吸顶

--- src/pages/List/Filter.vue
+++ src/pages/List/Filter.vue
@@ -1,89 +1,85 @@
 <template>
-  <section class="filter" :class="{fixed: isFixed}" :style="{top: isFixed ? `${fixedDistance}px` : 0}">
-    <ul class="menu">
-      <li v-for="(item, i) in dataSource" @click="switchPanel(i)" :class="{active: i == activeIndex[0]}">
-        {{1 < item.length ? item[activeIndex[1]].title : item[0].title}} <Icon v-if="1 < item.length" :class="{rotate: i == expandIndex}" value="chevron-up" class="icon">
-          </Icon>
-      </li>
-    </ul>
-    <!-- 菜单选项 -->
-    <ul v-if=" -1 < expandIndex && 1 < dataSource[expandIndex].length" v-show="isShow" class="dropdown">
-      <li v-for="(item, i) in dataSource[expandIndex]" @click="change(expandIndex, i)" :class="{active: i == activeIndex[1]}">
-        {{item.title}}
-      </li>
-    </ul>
-  </section>
+    <section class="filter">
+        <ul class="menu">
+            <template v-for="(item, i) in dataSource">
+                <li v-if="'list' == item.type" :key="i" @click="openMenu(i)" :class="{active: i == activeNavIndex}">
+                    {{item.children[activeOptionIndex].title}}
+                    <Icon :class="{rotate: i == activeNavIndex}" value="chevron-up" class="icon"></Icon>
+                </li>
+                <li v-else :key="i" @click="selectNavOption(i, item.value)" :class="{active: i == activeNavIndex}">
+                    {{item.title}}
+                </li>
+            </template>
+        </ul>
+        <!-- 菜单选项 -->
+        <ul v-if="'list' == dataSource[activeNavIndex].type && isOpenNavIndex == activeNavIndex && isOpen" class="dropdown">
+            <li v-for="(item, i) in dataSource[activeNavIndex].children" :key="item.title" @click="selectOption(i, item.value)">
+                {{item.title}}
+            </li>
+        </ul>
+    </section>
 </template>
 <script>
 export default {
-  name: 'FilterLayout',
+    name: 'FilterLayout',
 
-  props: {
-    scrollY: {
-      type: Number,
-      default: 0
+    props: {
+        scrollY: {
+            type: Number,
+            default: 0
+        },
+
+        isOpen: {
+            type: Boolean,
+            default: false
+        },
     },
 
-    isShow: {
-      type: Boolean,
-      default: false
-    },
-  },
-
-  data() {
-    return {
-      dataSource: [
-        [{ title: 'Default', value: 0 },
-          { title: 'Recomond', value: 1 },
-          { title: 'Hight To Low', value: 2 },
-          { title: 'Low To Hight', value: 3 }
-        ],
-        [{ title: 'Best Selling', value: 4 }],
-        [{ title: 'Newest', value: 5 }]
-      ],
-      isFixed: false,
-      offsetTop: 0, // 距离父容器顶部距离
-      fixedDistance: 0,
-      activeIndex: [0, 0], // 选中值的索引
-      expandIndex: -1,  // 当前打开的tabs
-    };
-  },
-
-  mounted() {
-    this.offsetTop = this.$el.offsetTop + this.$el.parentNode.offsetTop;
-    this.fixedDistance = this.$el.getBoundingClientRect().top - this.offsetTop;
-    this.fixedDistance = parseInt(this.fixedDistance);
-  },
-
-  methods: {
-    switchPanel(index) {
-      this.expandIndex = index;
-      if (1 < this.dataSource[index].length) {
-        this.$emit('expand-menu', { index, isMulti: true });
-      } else {
-        this.activeIndex = [index, 0];
-        this.$emit('change', { tabsIndex: index, optionsIndex: 0 })
-        this.$emit('expand-menu', { index, isMulti: false });
-      }
+    data() {
+        return {
+            dataSource: [
+                {
+                    type: 'list',
+                    children: [
+                        { type: 'item', title: 'Default', value: 0 },
+                        { type: 'item', title: 'Recomond', value: 1 },
+                        { type: 'item', title: 'Hight To Low', value: 2 },
+                        { type: 'item', title: 'Low To Hight', value: 3 }
+                    ]
+                },
+                { type: 'item', title: 'Best Selling', value: 4 },
+                { type: 'item', title: 'Newest', value: 5 }
+            ],
+            isOpenNavIndex: -1, // 被展开的导航索引
+            activeNavIndex: 0,
+            activeOptionIndex: 0,
+        };
     },
 
-    change(tabsIndex, optionsIndex) {
-      this.expandIndex = -1;
-      this.activeIndex = [tabsIndex, optionsIndex];
-      this.$emit('change', { tabsIndex, optionsIndex })
-    }
-  },
+    methods: {
+        /**
+         * 切换导航
+         */
+        openMenu(navIndex) {
+            this.isOpenNavIndex = navIndex;
+            this.activeNavIndex = navIndex;
+            this.$emit('open');
+        },
 
-  watch: {
-    scrollY(value) {
-      if (value > this.offsetTop) {
-        this.isFixed = true;
-      } else {
-        this.isFixed = false;
-        const { top } = this.$el.getBoundingClientRect();
-      }
-    }
-  },
+        selectNavOption(navIndex, value) {
+            this.activeNavIndex = navIndex;
+            this.activeOptionIndex = 0;
+            this.$emit('select', value);
+        },
+
+        selectOption(optionIndex, value) {
+            this.isOpenNavIndex = -1;
+            this.activeOptionIndex = optionIndex;
+            this.dataSource[this.activeNavIndex].isOpen = false;
+            this.$emit('select', value);
+        }
+    },
+
 }
 
 </script>
@@ -91,60 +87,51 @@ export default {
 @import '../../scss/theme.scss';
 $height:1rem;
 .filter {
-  position: absolute;
-  top: 0;
-  background: $background;
-  width: 100%;
-  &.fixed {
-    // transition:all .2s;
-    position: fixed;
-    z-index: 10;
-    top: 1.2rem;
-  }
-  ul.menu {
-    height: $height;
-    display: flex;
-    li {
-      flex: 1;
-      height: $height;
-      width: 100%;
-      line-height: $height;
-      display: block;
-      font-size: $big;
-      text-align: center;
-      border-color: $lightest;
-      border-style: solid;
-      border-width: 1px 0 1px 0;
-      &:last-child {
-        border-right-width: 0;
-      }
-
-      >.icon {
-        transition: all .6s;
-        &.rotate {
-          transform: rotate(180deg);
-        }
-      }
-
-      &.active {
-        color: $base;
-      }
-    }
-  }
-
-  ul.dropdown {
     background: $background;
-    position: relative;
-    z-index: 10;
-    li {
-      padding: 3*$gutter;
-      border-bottom: 1px solid $lightest;
-      font-size: $big;
-      &.active {
-        color: $base;
-      }
-    }
-  }
-}
+    width: 100%;
 
+    ul.menu {
+        height: $height;
+        display: flex;
+        li {
+            flex: 1;
+            height: $height;
+            width: 100%;
+            line-height: $height;
+            display: block;
+            font-size: $big;
+            text-align: center;
+            border-color: $lightest;
+            border-style: solid;
+            border-width: 1px 0 1px 0;
+            &:last-child {
+                border-right-width: 0;
+            }
+
+            >.icon {
+                transition: all .6s;
+                &.rotate {
+                    transform: rotate(180deg);
+                }
+            }
+
+            &.active {
+                color: $base;
+            }
+        }
+    } // 下拉菜单
+    ul.dropdown {
+        background: $background;
+        position: relative;
+        z-index: 10;
+        li {
+            padding: 3*$gutter;
+            border-bottom: 1px solid $lightest;
+            font-size: $big;
+            &.active {
+                color: $base;
+            }
+        }
+    }
+}
 </style>
