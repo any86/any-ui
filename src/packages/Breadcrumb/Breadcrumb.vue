654ce70ac8235d52ad03851c4f9552ff05a558a6
宁宁 <383514580@qq.com>
2017-08-11 12:04:10 +0800

    新建面包屑组件

---
+++ src/packages/Breadcrumb/Breadcrumb.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="component-breadcrumb">
+        <span v-for="item in dataSource">{{item.text}}</span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Breadcrumb',
+
+    props: {
+        dataSource: {
+            type: Array
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../../scss/theme.scss';
+.component-breadcrumb {
+    margin: $gutter;
+    span {
+        font-size: $small;
+        display: inline-block;
+        &.active {
+            color: $base;
+        }
+    }
+    span:not(:last-of-type) {
+        &:after {
+            color: $darkest;
+            content: '>';
+            margin: auto 5px;
+        }
+    }
+}
+</style>
