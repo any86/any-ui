9e83079373a4422408480303f14c1506a6c4f65c
宁宁 <383514580@qq.com>
2017-08-28 17:28:00 +0800

    新建

---
+++ src/packages/Tooltip/Tooltip.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="component-tooltip" v-on="$listeners">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Tooltip',
+
+    props: {
+        value: {
+            type: Array
+        }
+    },
+    data() {
+        return {
+            popper: null
+        }
+    },
+
+    mounted() {
+        var node = document.createElement('span');
+        node.id = 'tooltip'
+        document.body.appendChild(node);
+        var component = this.constructor;
+        var vm = new component().$mount('#' + node.id);
+    },
+
+    methods: {
+    },
+
+    watch: {
+
+    }
+};
+</script>
+<style scoped lang="scss">
+// @import '../../scss/theme.scss';
+// .component-tooltip {}
+</style>
