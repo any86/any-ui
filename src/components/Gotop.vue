1a4396900d6916508fb5c7d384415e565d0b5e79
宁宁 <383514580@qq.com>
2017-08-29 15:54:34 +0800

    替换iconfont

--- src/components/Gotop.vue
+++ src/components/Gotop.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="component-gotop">
-    <Icon value="chevron-up"></Icon>
-  </div>
+    <div class="component-gotop">
+        <i class="iconfont icon-less"></i>
+    </div>
 </template>
 <script>
 export default {
-  name: 'GoTop',
+    name: 'GoTop',
 }
 
 </script>
 <style lang="scss" scoped>
 @import '../scss/theme.scss';
 .component-gotop {
-  position: fixed;
-  bottom: 1.2rem;
-  right: .3rem;
-  z-index: 999;
-  background: rgba($darkest, .8);
-  text-align: center;
-  width: .8rem;
-  height: .8rem;
-  line-height: .8rem;
-  border-radius: .8rem;
-  font-size: $biggest;
-  color: $sub;
-  &:active{background: rgba($darkest, .5);}
-}
+    position: fixed;
+    bottom: 1.2rem;
+    right: .3rem;
+    z-index: 999;
+    background: rgba($darkest, .8);
+    text-align: center;
+    width: .8rem;
+    height: .8rem;
+    line-height: .8rem;
+    border-radius: .8rem;
+    font-size: $biggest;
+    color: $sub;
+    &:active {
+        background: rgba($darkest, .5);
+    }
 
+    i{width: .8rem;height: .8rem;display: block;font-size: $biggest;}
+}
 </style>
