376988a45b1a5094faccabb46a303e8dca1b433e
宁宁 <383514580@qq.com>
2017-09-08 17:23:51 +0800

    增加上传mock

--- mock.js
+++ mock.js
@@ -6,6 +6,51 @@ var express = require("express");
 var app = express();
 var Mock = require("mockjs");
 
+/* 上传 */
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// 上传路径
+app.use("/uploads", express.static("./uploads/"));
+// 上传功能
+var uploadDir = "./uploads";
+
+app.post("/upload", function(req, res) {
+    // 建立上传文件夹
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+    }
+
+    var form = new multiparty.Form({ uploadDir });
+
+    form.on("error", function(err) {
+        console.log("Error parsing form: " + err.stack);
+    });
+
+    form.parse(req, function(err, fields, files) {
+        var filesTmp = JSON.stringify(files, null, 2);
+        if (err) {
+            console.log("parse error: " + err);
+            res.send("写文件操作失败。");
+        } else {
+            if (!!files.file[0]) {
+                var result = {
+                    status: 1,
+                    data: {
+                        url: files.file[0].path,
+                        id: Mock.Random.natural()
+                    }
+                };
+            } else {
+                var result = {
+                    status: 0,
+                    message: "错误了啊!"
+                };
+            }
+            res.json(result);
+        }
+    });
+});
+
 app.get("/goods", function(req, res) {
     var data = Mock.mock({
         "array|10": [
