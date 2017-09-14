47334c8d819c0a2da2170cee002ffc878a3e1609
宁宁 <383514580@qq.com>
2017-09-14 11:53:36 +0800

    saveCharm改版uploadCharm

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
-import store from "@/store";
-import Axios from "axios";
+import store from '@/store';
+import Axios from 'axios';
 const axios = Axios.create({
     headers: {
-        "api-store": "default",
-        "api-cache": 1
+        'api-store': 'default',
+        'api-cache': 1
     }
 });
 
@@ -13,7 +13,7 @@ axios.interceptors.request.use(
         config.headers = {
             ...config.headers,
             ...{
-                "api-currency": store.state.currency
+                'api-currency': store.state.currency
             }
         };
         return config;
@@ -22,64 +22,64 @@ axios.interceptors.request.use(
         return Promise.reject(error);
     }
 );
-const getFont = params => axios.get("./static/font.json", { params });
+const getFont = params => axios.get('./static/font.json', { params });
 
 // 首页
-const getIndexPage = params => axios.get("./static/index.json", { params });
+const getIndexPage = params => axios.get('./static/index.json', { params });
 // 列表页
-const getListPage = params => axios.get("./static/list.json", { params });
+const getListPage = params => axios.get('./static/list.json', { params });
 // 购物车页面
-const getCartPage = params => axios.get("./static/cart.json", { params });
+const getCartPage = params => axios.get('./static/cart.json', { params });
 // 存储收获地址
-const saveAddress = params => axios.post("./mock/success", params);
+const saveAddress = params => axios.post('./mock/success', params);
 // 商品列表
 // const getGoodsList = params => axios.get('/api/rest/catalog/product?category=7', { params });
 
-const getGoodsList = params => axios.get("./mock/goods", { params });
+const getGoodsList = params => axios.get('./mock/goods', { params });
 
 const getGoodsListBySku = params =>
-    axios.get("/api/rest/catalog/product", { params });
+    axios.get('/api/rest/catalog/product', { params });
 // 登录页
-const login = params => axios.post("/oauth/login", params);
+const login = params => axios.post('/oauth/login', params);
 // 购物车相关
 // const getGoodsListOfCart = params => axios.get('./static/cartGoodsList.json', { params });
 const getGoodsListOfCart = params =>
-    axios.get("/api/rest/cart/item", { params });
+    axios.get('/api/rest/cart/item', { params });
 const getCartRecommend = params =>
     axios.get(
-        "/api/rest/catalog/product?skus[]=YB1353&skus[]=YB1264&skus[]=XS1386&skus[]=XS1325&skus[]=YB1384",
+        '/api/rest/catalog/product?skus[]=YB1353&skus[]=YB1264&skus[]=XS1386&skus[]=XS1325&skus[]=YB1384',
         { params }
     );
 const getCartGiftList = params =>
-    axios.get("./static/cartGoodsList.json", { params });
-const addGoodsToCart = data => axios.post("/api/rest/cart/item", data);
-const editGoodsOfCart = data => axios.put("/api/rest/cart/item", data);
+    axios.get('./static/cartGoodsList.json', { params });
+const addGoodsToCart = data => axios.post('/api/rest/cart/item', data);
+const editGoodsOfCart = data => axios.put('/api/rest/cart/item', data);
 const removeGoodsFromCart = data =>
-    axios.delete("/api/rest/cart/item", { data });
-const getTotalOfCart = params => axios.get("/api/rest/cart/total", { params });
+    axios.delete('/api/rest/cart/item', { data });
+const getTotalOfCart = params => axios.get('/api/rest/cart/total', { params });
 // 优惠券
-const useCoupon = data => axios.post("/api/rest/cart/coupon", data);
-const restoreCoupon = data => axios.delete("/api/rest/cart/coupon", { data });
+const useCoupon = data => axios.post('/api/rest/cart/coupon', data);
+const restoreCoupon = data => axios.delete('/api/rest/cart/coupon', { data });
 // 国家 / 省(洲) / 市
-const getCountry = () => axios.get("/api/rest/directory/country");
-const getRegion = params => axios.get("/api/rest/directory/region", { params });
+const getCountry = () => axios.get('/api/rest/directory/country');
+const getRegion = params => axios.get('/api/rest/directory/region', { params });
 
 // 地址相关
 const getShippingAddress = params =>
-    axios.get("/api/rest/checkout/address/shipping", { params });
+    axios.get('/api/rest/checkout/address/shipping', { params });
 const saveShippingAddress = data =>
-    axios.post("/api/rest/checkout/address/shipping", data);
+    axios.post('/api/rest/checkout/address/shipping', data);
 
 // 运输费用
 const getShippingFee = param =>
-    axios.get("/api/rest/checkout/shipping", { params });
+    axios.get('/api/rest/checkout/shipping', { params });
 
 // 保存charm
-const saveCharm = data => axios.post("./mock/upload", data);
+const uploadCharm = data => axios.post('./mock/upload', data);
 
 // 暴露
 export default {
-    saveCharm,
+    uploadCharm,
     getFont,
     getShippingFee,
     saveShippingAddress,
