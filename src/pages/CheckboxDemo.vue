c31ed0260b7206c511f06f9b80619912a553c6fe
宁宁 <383514580@qq.com>
2017-09-26 17:08:31 +0800

    优化样式

---
+++ src/pages/CheckboxDemo.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-scroll-view v-model="top" class="full-screen">
+        <v-group>
+            <template slot="header">
+                正常情况 - {{select1}}
+            </template>
+
+            <v-cell>
+                <v-checkbox v-model="select1" :true-value="1" :false-value="2">选项1</v-checkbox>
+            </v-cell>
+
+            <v-cell>
+                <v-checkbox v-model="select2" :true-value="1" :false-value="2">选项2</v-checkbox>
+            </v-cell>
+
+        </v-group>
+
+        <v-group>
+            <template slot="header">
+                禁用状态
+            </template>
+            <v-cell>
+                <v-checkbox v-model="select3" :true-value="1" :false-value="2" :disabled="true">选项1</v-checkbox>
+            </v-cell>
+            <v-cell>
+                <v-checkbox v-model="select4" :true-value="1" :false-value="2" :disabled="true">选项2</v-checkbox>
+            </v-cell>
+        </v-group>
+    </v-scroll-view>
+</template>
+<script>
+import VScrollView from '@/packages/ScrollView/ScrollView';
+import VCheckbox from '@/packages/Checkbox/Checkbox';
+import VButton from '@/packages/Button/Button';
+import VCell from '@/packages/Cell/Cell';
+import VGroup from '@/packages/Group/Group';
+import VCircle from '@/packages/Loading/Circle';
+
+export default {
+    name: 'CheckboxDemo',
+
+    data() {
+        return {
+            is1: false,
+            select1: 1,
+            select2: 1,
+            select3: 1,
+            select4: 2,
+            top: 0,
+        };
+    },
+
+    methods: {
+        click(e) {
+        }
+    },
+
+    components: {
+        VCell, VCheckbox, VGroup, VButton, VCircle, VScrollView
+    }
+}
+</script>
+<style scoped lang="scss">
+@import '../scss/theme.scss';
+.m-icon {
+    background: url(../assets/close.svg) center center;
+    background-size: 100%;
+    width: 32px;
+    height: 32px;
+    display: inline-block;
+}
+</style>
