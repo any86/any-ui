4de16d38d777e34cb5014d0abfe46018ad0212a8
宁宁 <383514580@qq.com>
2017-09-27 11:05:54 +0800

    创建svg的关闭按钮

---
+++ src/packages/Icon/Close.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg v-on="$listeners" class="icon" style="" :viewBox="`0 0 ${size} ${size}`" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" :size="size" :height="size">
+        <line x1="10" y1="10" :x2="size-10" :y2="size-10" :stroke-width="(size-10)/20" />
+        <line :x1="size-10" y1="10" x2="10" :y2="size-10" :stroke-width="(size-10)/20" />
+    </svg>
+</template>
+<script>
+export default {
+    name: 'IconClose',
+
+    props: {
+        size: {
+            default: 40
+        }
+    }
+
+}
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+line {
+    stroke: $light;
+}
+</style>
