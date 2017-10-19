5b2ed191d725082c493ef4123586fb9871538fae
宁宁 <383514580@qq.com>
2017-10-19 11:59:25 +0800

    完善scroller

--- src/packages/Scroller/Scroller.vue
+++ src/packages/Scroller/Scroller.vue
@@ -43,22 +43,6 @@ export default {
             default: () => ({ scrollLeft: 0, scrollTop: 0 }) // 注意返回对象, 要用括号包起来
         },
 
-        minScrollTop: {
-            type: Number
-        },
-
-        maxScrollTop: {
-            type: Number
-        },
-
-        minScrollLeft: {
-            type: Number
-        },
-
-        maxScrollLeft: {
-            type: Number
-        },
-
         isLockX: {
             type: Boolean,
             default: true
@@ -74,7 +58,7 @@ export default {
             default: 15
         },
 
-        hasBuffer: {
+        hasBufferMove: {
             type: Boolean,
             default: true
         },
@@ -90,11 +74,6 @@ export default {
             default: true
         },
 
-        isFreeScroll: {
-            type: Boolean,
-            default: false
-        },
-
         bodyStyle: {
             type: Object,
             default: () => { }
@@ -107,7 +86,7 @@ export default {
 
         maxHolderTime: {
             type: Number,
-            default: 100
+            default: 300
         },
 
         isBindBody: {
@@ -118,22 +97,16 @@ export default {
 
     data() {
         return {
-            isAnimateMoving: false,
+            isBufferMoving: false,
             moveRatio: 1,
             speed: 0,
             startTime: 0,
             endTime: 0,
-
-            startPointY: 0, // 手指坐标
-            startPointX: 0,
-
-            directionLock: 'n',
-
             transitionDuration: 0,
-
             viewWidth: 0,
             viewHeight: 0,
-
+            startPointY: 0, // 手指坐标
+            startPointX: 0,
             translateX: 0,
             translateY: 0,
             maxTranslateX: 0,
@@ -154,13 +127,7 @@ export default {
         this.addEvents(this.isBindBody ? this.$refs.body : this.$el);
         this.updateSize();
         window.addEventListener('resize', this.updateSize);
-        if (!this.isLockX && this.isLockY) {
-            this.directionLock = 'y'
-        } else if (this.isLockX && !this.isLockY) {
-            this.directionLock = 'x'
-        } else {
-            this.directionLock = 'n'
-        }
+
     },
 
     methods: {
@@ -200,41 +167,40 @@ export default {
         },
 
         touchstart(e) {
+            // stopPropagation | preventDefault必须放在顶部, 不然下面的return false 会阻止代码运行
+            e.stopPropagation();
+            // 阻止浏览器默认行为
+            this.preventDefault && e.preventDefault();
+
             // 禁用touch事件
             if (this.disableTouch) return;
+
+            // ========== 计算滑动 ========== 
             const point = e.touches ? e.touches[0] : e;
             this.transitionDuration = 0;
+
+            // 记录触摸时间
             this.startTime = getTime();
+
             // 记录当前触碰点在屏幕上的坐标
             this.startPointY = point.pageY;
             this.startPointX = point.pageX;
 
-            // 判断起点是否边缘
-            let edge = '';
-            // 四个角度肯定是不精准了, 不过暂时觉得没什么实际意义
-            if (this.edgeThreshold > point.pageX) {
-                edge = 'left';
-            } else if (this.viewWidth - this.edgeThreshold < point.pageX) {
-                edge = 'right';
-            } else if (this.edgeThreshold > point.pageY) {
-                edge = 'top';
-            } else if (this.viewHeight - this.edgeThreshold < point.pageY) {
-                edge = 'bottom';
-            }
-
-            // 记录滑动前scroll-body的translate信息
+            // 记录滑动当前translate信息
             this.startTranslateY = this.translateY;
             this.startTranslateX = this.translateX;
-            e.stopPropagation();
-            // 阻止浏览器默认行为
-            this.preventDefault && e.preventDefault();
-            this.$emit('scroll-start', { ...this.moveData, e, edge });
+
+            // 定义组件事件
+            this.$emit('scroll-start', { ...this.moveData, e });
         },
+
         touchmove(e) {
-            // 禁用touch事件
+            e.stopPropagation();
+            this.preventDefault && e.preventDefault();
+            // 禁用touch事件[停止运行]
             if (this.disableTouch) return;
 
-            // x/y都lock了
+            // x/y都lock了[停止运行]
             if (this.isLockX && this.isLockY) return;
 
             // 这句没看懂iscroll的意义
@@ -252,47 +218,32 @@ export default {
             if (this.sensitivity > absDeltaY && this.sensitivity > absDeltaX) return;
 
             // ========== 计算滑动 ==========
+            if (!this.isLockX && this.isLockY) {
+                // X轴可拖拽
 
-            // 确定手指滑动方向
-            // 锁定手指滑动距离小的轴线方向的页面滚动
-            if (absDeltaX > absDeltaY + this.directionLockThreshold) {
-                this.directionLock = 'y';
-            } else if (absDeltaY > this.directionLockThreshold + absDeltaX) {
-                this.directionLock = 'x';
-            } else {
-                // this.directionLock = 'n';
-            }
+                // 如果x轴向移动没超过y轴+阈值[停止运行]
+                if (absDeltaX < absDeltaY + this.directionLockThreshold) return;
 
-            // 对锁定方向的增量(delta)归0
-            if ('y' == this.directionLock) {
-                deltaY = 0;
-            } else if ('x' == this.directionLock) {
-                deltaX = 0;
-            }
+                // 如果X轴向内容没有溢出[停止运行]
+                if (!this.isOverflowX) return;
 
-            // 如果锁定X轴的移动
-            // 如果锁定Y
-            // 自由移动
-            if (this.isLockX && !this.isLockY) {
-                // 当scroll-body的位置超出边界, 那么滑动距离 : 手指移动距离 = 1 : 2
-                this.moveRatio = this.isOutOfYLimit ? .5 : 1;
-                this.translateY = this.startTranslateY + deltaY * this.moveRatio;
-            } else if (this.isLockY && !this.isLockX) {
-                this.moveRatio = this.isOutOfXLimit ? .5 : 1;
+                // 如果超出边界[停止运行]
+                // code...
+
+                // 超出边界减慢拖拽移动速度
+                this.moveRatio = this.isOutOfXLimit ? .3 : 1;
                 this.translateX = this.startTranslateX + deltaX * this.moveRatio;
-            }
-
-            // 控制translateX在范围内
-            if(this.maxTranslateX < 0 - this.translateX) {
-                this.translateX = 0 - this.maxTranslateX;
-            } else if(0 < this.translateX) {
-                this.translateX = 0;
-            }
-            // 控制translateY在范围内
-            if(this.maxTranslateY < 0 - this.translateY) {
-                this.translateY = 0 - this.maxTranslateY;
-            }else if(0 < this.translateY) {
-                this.translateY = 0;
+            } else if (this.isLockX && !this.isLockY) {
+                // Y轴可拖拽
+                if (absDeltaY < absDeltaX + this.directionLockThreshold) return;
+                if (!this.isOverflowY) return;
+                this.moveRatio = this.isOutOfYLimit ? .3 : 1;
+                this.translateY = this.startTranslateY + deltaY * this.moveRatio;
+            } else {
+                // 自由移动, 最上面已经对禁用移动判断过了
+                this.moveRatio = 1;
+                this.translateX = this.startTranslateX + deltaX * this.moveRatio;
+                this.translateY = this.startTranslateY + deltaY * this.moveRatio;
             }
 
             // 当手指一直按住突然拖动, 那么重置起始值
@@ -303,37 +254,39 @@ export default {
                 this.startTranslateY = this.translateY;
                 this.startTranslateX = this.translateX;
             }
-
-            e.stopPropagation();
-            // 阻止默认行为(页面滚动)
-            this.preventDefault && e.preventDefault();
+            // 派发组件事件
             this.$emit('input', { scrollTop: -this.translateY, scrollLeft: -this.translateX });
             this.$emit('scroll-move', { ...this.moveData, e });
         },
 
         touchend(e) {
+            e.stopPropagation();
+            this.preventDefault && e.preventDefault();
             // 禁用touch事件
             if (this.disableTouch) return;
             this.transitionDuration = 500;
             this.endTime = getTime();
             const point = e.changedTouches ? e.changedTouches[0] : e;
 
-            // buffer
-            if (this.hasBuffer) {
-                // this.bufferMove(point);
+            // 缓冲运动
+            if (this.hasBufferMove) {
+                this.bufferMove(point);
+                // 派发事件
+                this.$emit('buffer-move', true);
             }
-            e.stopPropagation();
-            this.preventDefault && e.preventDefault();
-            this.$emit('update:isAnimateMoving', true);
+
+            // 派发事件     
             this.$emit('input', { scrollTop: -this.translateY, scrollLeft: -this.translateX });
             this.$emit('scroll-leave', { ...this.moveData, e });
         },
 
         transitionend() {
             if (this.disableTouch) return;
-            // this.$emit('update:isAnimateMoving', false);
+            if (this.hasBufferMove) {
+                this.$emit('buffer-move', false);
+            }
             this.$nextTick(() => {
-                this.isAnimateMoving = false;
+                this.isBufferMoving = false;
                 this.$emit('scroll-end', { scrollTop: -this.translateY, scrollLeft: -this.translateX });
             });
         },
@@ -342,24 +295,24 @@ export default {
          */
         bufferMove(point) {
             const costTime = this.endTime - this.startTime;
-             const deltaX = point.pageY - this.startTranslateX;
-            const deltaY = point.pageY - this.startPointX;
-            this.isAnimateMoving = true;
-            if ('y' == this.directionLock && deltaX > deltaY + 15) {
+            const deltaX = point.pageY - this.startPointX;
+            const deltaY = point.pageY - this.startPointY;
+            this.isBufferMoving = true;
+            if (!this.isLockX && this.isLockY) {
+                // X轴可拖拽
                 if (this.maxHolderTime > costTime) {
-                   
                     const speedX = deltaX / costTime;
                     this.translateX += speedX * 1000;
                 }
                 // 自动复位
                 this.hasReset && this.resetX();
-            } else if ('x' == this.directionLock && deltaY > deltaX + 15) {
+            } else if (this.isLockX && !this.isLockY) {
+                // Y轴可拖拽
                 if (this.maxHolderTime > costTime) {
-                    
                     const speedY = deltaY / costTime;
                     this.translateY += speedY * 1000;
                 }
-                //自己复位
+                // 自动复位
                 this.hasReset && this.resetY();
             }
         },
@@ -384,6 +337,14 @@ export default {
     },
 
     computed: {
+        isOverflowX() {
+            return 0 < this.maxTranslateX;
+        },
+
+        isOverflowY() {
+            return 0 < this.maxTranslateY;
+        },
+
         isOutOfTopLimit() {
             return 0 < this.translateY;
         },
@@ -426,7 +387,7 @@ export default {
         value: {
             deep: true,
             handler(value) {
-                // if (!this.isAnimateMoving) {
+                // if (!this.isBufferMoving) {
                 this.transitionDuration = 500;
                 this.translateY = -value.scrollTop;
                 this.translateX = -value.scrollLeft;
