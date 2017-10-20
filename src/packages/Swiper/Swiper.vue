dd1845daa98fd7c9898f346bf58ea3745232cfa8
宁宁 <383514580@qq.com>
2017-10-20 14:31:29 +0800

    基本完成

--- src/packages/Swiper/Swiper.vue
+++ src/packages/Swiper/Swiper.vue
@@ -8,13 +8,22 @@
 </template>
 <script>
 import Swiper from 'swiper';
-import 'swiper/dist/css/swiper.css';
 export default {
     name: 'Swiper',
 
     props: {
         options: {
             type: Object
+        },
+
+        delay: {
+            type: Number,
+            default: 3000
+        },
+
+        loop: {
+            type: Boolean,
+            default: false
         },
 
         hasPagination: {
@@ -35,24 +44,51 @@ export default {
     },
 
     mounted() {
-        // 生成实例
-        this.swiper = new Swiper(this.$el, {
-            slidesPerView: 1,
-            pagination: '.swiper-pagination',
-            touchMoveStopPropagation: true
-        });
+        // 合并配置
+        let opts = Object.assign(
+            {
+                initialSlide: this.value,
+                loop: this.loop,
+                autoplay: {
+                    delay: this.delay
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets'
+                    // modifierClass: 'atom-swiper__pagination-',
+                    // bulletClass: 'pagination-bullet',
+                    // bulletActiveClass: 'pagination__bullet--active',
+                    // currentClass: 'atom-swiper__pagination--current',
+                    // totalClass: 'atom-swiper__pagination--total',
+                    // hiddenClass: 'atom-swiper__pagination--hidden',
+                    // progressbarFillClass: 'pagination__progressbar--fill',
+                    // clickableClass: 'atom-swiper__pagination--clickable'
+                }
+            },
+            this.options
+        );
+        this.swiper = new Swiper(this.$el, opts);
 
         this.swiper.on('slideChange', swiper => {
-            dir(swiper);
-            this.$emit('input', this.value);
-            this.$emit('change-start', swiper);
+            this.$emit('slide-change', this.swiper);
+            this.$emit('input', this.swiper.realIndex);
+        });
+
+        this.swiper.on('slideChangeTransitionStart', swiper => {
+            this.$emit('slide-change-transition-start', this.swiper);
+        });
+
+        this.swiper.on('slideChangeTransitionEnd', swiper => {
+            this.$emit('slide-change-transition-end', this.swiper);
         });
     },
 
     watch: {
-        // value(value) {
-        //     this.swiper.slideTo(value);
-        // }
+        value(value) {
+            if (!this.swiper.animating) {
+                this.swiper.slideTo(value);
+            }
+        }
     },
 
     destroy() {
@@ -61,6 +97,15 @@ export default {
     }
 };
 </script>
-<style scoped lang="scss">
+<style lang="scss">
 @import '../../scss/theme.scss';
+@import '~swiper/dist/css/swiper.css';
+.swiper-pagination-bullet {
+    
+    &-active{
+        border-radius: $big;
+        background: rgba($base, .7);
+        width: $big;;
+    }
+}
 </style>
