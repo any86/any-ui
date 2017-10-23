183a07fd3c06fbaeb533cd165207b7b76f712070
宁宁 <383514580@qq.com>
2017-10-23 17:28:45 +0800

    完成

---
+++ src/packages/Segment/Segment.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="atom-segment" role="tablist" v-on="$listeners">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Segment',
+
+    props: {
+        value: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    data() {
+        return { count: 0 };
+    }
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+$height: 0.3rem;
+.atom-segment {
+    position: relative;
+    display: table;
+    margin: auto;
+    text-align: center;
+    border: 1px solid $base;
+    border-radius: $borderRadius;
+}
+</style>
---
+++ src/packages/Segment/SegmentItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <span @click="setActive" class="atom-segment-control__item" :class="{'atom-segment-control__item--active': $parent.value == index}">
+        <slot></slot>
+    </span>
+</template>
+<script>
+export default {
+    name: 'SegmentItem',
+
+    data() {
+        return { index: 0 };
+    },
+
+    mounted() {
+        this.index = this.$parent.count;
+        this.$parent.count++;
+    },
+
+    methods: {
+        setActive() {
+            this.$parent.activeWidth = this.$el.scrollWidth;
+            this.$parent.$emit('input', this.index);
+        }
+    }
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.atom-segment-control__item {
+    position: relative;
+    z-index: 2;
+    display: table-cell;
+    text-align: center;
+    padding: $gutter / 3 $gutter;
+    color: $base;
+    border-right: 1px solid $base;
+    &:last-child {
+        border-right: none;
+    }
+    &--active {
+        transition: all 200ms;
+        background: $base;
+        color: $sub;
+    }
+}
+</style>
