360201b655d54f332394519932dc503b9fb66925
宁宁 <383514580@qq.com>
2017-10-24 15:26:23 +0800

    调整节流函数的调用位置

--- src/packages/LazyLoad/LazyLoad.vue
+++ src/packages/LazyLoad/LazyLoad.vue
@@ -4,10 +4,12 @@
 <script>
 // 借鉴了vue-lazyload, 我这就一简化版
 // https://github.com/hilongjw/vue-lazyload
-// IntersectionObserver不知道兼容性, 稍后测验
+// IntersectionObserver不知道兼容性, 稍后测验, caniuse说支持安卓>=5
 // https://developer.mozilla.org/zh-CN/docs/Web/API/Intersection_Observer_API
-import { getWidth, getHeight, getScrollParent } from '@/utils/dom';
-import debounce from 'lodash/debounce';
+// 未来想借助了indexeddb对每个图片标记id, 让加载过的图片可以省去一些流程判断.
+// 还要加入对每个图片加载事件的统计事件, 方便找出加载慢的图片
+import { getWidth, getHeight, getScrollParent, getTime } from '@/utils/dom';
+import throttle from 'lodash/throttle';
 export default {
     name: 'LazyLoad',
 
@@ -29,17 +31,17 @@ export default {
             }
         },
 
-        isLoad: {
-            type: Boolean,
-            default: false
+        throttleTime: {
+            type: Number,
+            default: 200
         },
 
         attempt: {
             type: Number,
-            default: 1
+            default: 3
         },
 
-        errorImage: {
+        errorImg: {
             type: String
         },
 
@@ -58,18 +60,28 @@ export default {
                     'resize',
                     'animationend',
                     'transitionend',
+                    'webkitAnimationend',
+                    'webkitTransitionend',
                     'touchmove'
                 ];
             }
+        },
+
+        refreshTime: {
+            type: Number
+            // default: getTime()
+            // 一般只有出现2组图片的父元素是通过v-show来控制的情况才需要
+            // 如果不愿意用refreshTime去强制触发刷新,
+            // 那么就需要在图片元素或者父元素上使用v-if进行控制渲染开始的时机, 来触发mounted中的代码.
         }
     },
 
     data() {
         return {
             status: 'ready',
-            rect: {},
             url: '',
-            loadTime: 0,
+            costTime: 0,
+            attemptCount: 0,
             scrollParentNode: null
         };
     },
@@ -81,7 +93,7 @@ export default {
         this.events.forEach(eventName => {
             this.scrollParentNode.addEventListener(
                 eventName,
-                debounce(this.loadInViewImg, 200)
+                this.loadInViewImg
             );
         });
         // 初始化加载图片
@@ -95,16 +107,19 @@ export default {
          * 否则移除监听
          */
         loadInViewImg() {
-            if ('done' === this.status) {
-                this.scrollParentNode.removeEventListener(
-                    'scroll',
-                    this.loadInViewImg
-                );
-            } else {
-                if (this.checkInView()) {
-                    this.loadImg();
+            // 不cancel能自动释放吗?
+            throttle(() => {
+                if ('loaded' === this.status) {
+                    this.scrollParentNode.removeEventListener(
+                        'scroll',
+                        this.loadInViewImg
+                    );
+                } else if ('ready' === this.status) {
+                    if (this.checkInView()) {
+                        this.loadImg();
+                    }
                 }
-            }
+            }, 200)();
         },
 
         checkInView() {
@@ -125,28 +140,51 @@ export default {
         },
 
         loadImg() {
-            this.status = 'loading';
-            // if(img.compulet);
-            let img = new Image();
+            const startTime = getTime();
+            var img = new Image();
             img.src = this.src;
+            if (this.attempt > this.attemptCount) {
+                this.attemptCount++;
+                // 这里有每个图都有placeholder, 所以不能判断img.complete,
+                // 也不用判断, 因为每次记载成功后, 第二次加载会自动解除触发绑定的事件
+                this.status = 'loading';
+                // 加载成功
+                img.onload = e => {
+                    this.status = 'loaded';
+                    this.url = this.src;
+                    this.costTime = getTime() - startTime;
+                    this.$emit('loaded', {
+                        width: img.naturalWidth,
+                        height: img.naturalHeight,
+                        url: this.url,
+                        costTime: this.costTime
+                    });
+                    img = null;
+                };
 
-            // 加载成功
-            img.onload = e => {
-                this.status = 'done';
-                this.url = this.src;
-                this.loadTime = e.timeStamp;
-                img = null;
-            };
-
-            // 错误处理
-            img.onerror = e => {
+                // 错误处理
+                img.onerror = e => {
+                    this.status = 'attempt';
+                    this.$emit('attempt', {
+                        url: this.url,
+                        costTime: this.costTime
+                    });
+                    this.loadImg();
+                };
+            } else {
+                this.url = this.errorImg;
                 this.status = 'fail';
-            };
+                this.$emit('fail', {
+                    url: this.url,
+                    costTime: this.costTime
+                });
+                img = null;
+            }
         }
     },
 
     watch: {
-        isLoad(value) {
+        refreshTime() {
             this.loadInViewImg();
         }
     },
@@ -166,10 +204,18 @@ export default {
 .atom-lazyload {
     width: 100%;
     display: block;
-    &[lazy='loading'] {
-        display: block;
+    // &[lazy='loading'] {
+
+    // }
+    &[lazy='loaded'] {
+        animation: fadeIn 1s;
     }
-    &[lazy='done'] {
+
+    // &[lazy='attempt'] {
+
+    // }
+
+    &[lazy='fail'] {
         animation: fadeIn 1s;
     }
 }
