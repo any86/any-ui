f237fa1e949c4936abd9495cb8060a1231b0e0d4
宁宁 <383514580@qq.com>
2017-10-27 16:15:29 +0800

    :zap:新建tag组件

---
+++ src/packages/Tag/Tag.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: 'Tag',
+
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+
+        isGhost: {
+            type: Boolean,
+            default: true
+        }
+    },
+
+    render(createElement) {
+        return createElement(
+            'span',
+            { class: ['atom-tag', `atom-tag${this.isGhost ? '-ghost' : ''}--${this.type}`] },
+            this.$slots.default
+        );
+    }
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.atom-tag {
+    padding: 2px $gutter / 2;
+    border-radius: $borderRadius;
+}
+
+@each $key, $value in $theme_colors {
+    .atom-tag-ghost--#{$key} {
+        border: 1px solid $value;
+        color: $value;
+    }
+}
+
+@each $key, $value in $theme_colors {
+    .atom-tag--#{$key} {
+        border: 1px solid $value;
+        background: $value;
+        color: $sub;
+    }
+}
+
+</style>
