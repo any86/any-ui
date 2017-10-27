b59730d87a6d571124b60b9837cae26329bf1211
宁宁 <383514580@qq.com>
2017-10-27 16:14:35 +0800

    :zap:新建分割线组件

---
+++ src/packages/Divider/Divider.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'Divider',
+
+    functional: true,
+
+    render(createElement) {
+        return createElement('div', {class: 'atom-divider'});
+    }
+
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.atom-divider {
+    margin: 0;
+    height: 1px;
+    border: none;
+    background-color: rgba(0, 0, 0, 0.12);
+    width: 100%;
+    overflow: visible;
+}
+</style>
