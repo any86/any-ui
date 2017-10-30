bfad14898067ad233188d0a717952f8a15aa3c86
宁宁 <383514580@qq.com>
2017-10-30 17:11:06 +0800

    取消slot, 感觉实际情况下很少用文字, 或者说其实都可以不用文字

---
+++ src/packages/Spinner/ThreeDots.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="atom-three-dots">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Spinner'
+};
+</script>
+
+<style scoped lang=scss>
+@import '../../scss/theme.scss';
+@mixin loading() {
+    @at-root& {
+        overflow: hidden;
+        margin: 0.5rem auto;
+        width: 150px;
+        text-align: center;
+        > div {
+            width: 20px;
+            height: 20px;
+            background: $light;
+            border-radius: 100%;
+            display: inline-block;
+            animation: bouncedelay 1.4s infinite ease-in-out;
+            /* Prevent first frame from flickering when animation starts */
+            animation-fill-mode: both;
+        }
+        .bounce1 {
+            animation-delay: -0.32s;
+        }
+        .bounce2 {
+            animation-delay: -0.16s;
+        }
+    }
+    @keyframes bouncedelay {
+        0%,
+        80%,
+        100% {
+            transform: scale(0);
+        }
+        40% {
+            transform: scale(1);
+        }
+    }
+}
+
+.atom-three-dots {
+    @include loading;
+}
+</style>
