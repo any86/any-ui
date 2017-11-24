cfcc3ea18977b73bb44cf5854cb8cb3f50f11b26
宁宁 <383514580@qq.com>
2017-11-24 11:42:58 +0800

    全屏组件

---
+++ src/packages/View/View.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="atom-view">
+        <slot></slot>
+    </main>
+</template>
+<script>
+export default {
+    name: 'View'
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.atom-view {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+</style>
