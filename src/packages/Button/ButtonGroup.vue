f1184c4bf5b2298f5a08fa3237c4a17ab85017fd
宁宁 <383514580@qq.com>
2017-12-02 14:36:35 +0800

    :whale: 新建btngroup组件

---
+++ src/packages/Button/ButtonGroup.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'ButtonGroup',
+
+    render(h) {
+        return h(
+            'div',
+            {
+                class: 'atom-btn-group'
+            },
+            this.$slots.default
+        );
+    }
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/theme.scss';
+.atom-btn-group {
+    display: flex;
+    >>>.atom-btn {
+        flex: 1;
+        border-radius: 0;
+
+        &:not(:last-child) {
+            border-right: none;
+        }
+    }
+}
+</style>
---
+++ src/pages/ButtonGroupDemo.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-scroll-view class="fill-sm">
+        <v-button-gorup>
+            <v-button :is-ghost="true" type="warning">warning</v-button>
+            <v-button :is-ghost="true" type="primary">primary</v-button>
+            <v-button :is-ghost="true" type="success">success</v-button>
+        </v-button-gorup>
+
+        <v-button-gorup class="gutter-top">
+            <v-button :is-ghost="true" type="info">info</v-button>
+            <v-button :is-loading="true" :is-ghost="true" type="danger">danger</v-button>
+        </v-button-gorup>
+    </v-scroll-view>
+</template>
+<script>
+import VScrollView from '@/packages/ScrollView/ScrollView';
+import VButton from '@/packages/Button/Button';
+import VButtonGorup from '@/packages/Button/ButtonGroup';
+
+export default {
+    name: 'ButtonGroupDemo',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {},
+
+    components: {
+        VButton,
+        VButtonGorup,VScrollView
+    }
+};
+</script>
+<style scope lang="scss">
+@import '../scss/theme.scss';
+</style>
