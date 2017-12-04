089a0156c46bfca00a9e5f54155f2470b8c5e11e
宁宁 <383514580@qq.com>
2017-12-04 15:05:27 +0800

    :whale: 取消js计算高度, 采用vh单位

--- src/packages/Dialog/Dialog.vue
+++ src/packages/Dialog/Dialog.vue
@@ -2,11 +2,11 @@
     <v-mask :isShow="isShow" :portal="portal" @update:isShow="close">
         <transition name="zoom">
             <div v-show="isShow" :class="['atom-dialog-' + align]" class="atom-dialog">
-                <header>
+                <header v-if="this.$slots.header">
                     <slot name="header"></slot>
                 </header>
 
-                <main :style="{maxHeight: windowHeight * 0.618 + 'px'}">
+                <main class="atom-dialog__body">
                     <slot></slot>
                 </main>
 
@@ -45,21 +45,7 @@ export default {
         }
     },
 
-    data() {
-        return {
-            windowHeight: 0
-        };
-    },
-
-    mounted() {
-        this.getWindowHeight();
-        window.addEventListener('resize', this.getWindowHeight);
-    },
-
     methods: {
-        getWindowHeight() {
-            this.windowHeight = getHeight(window);
-        },
 
         close() {
             this.$emit('update:isShow', false);
@@ -87,24 +73,25 @@ export default {
     width: 90%;
     max-width: 640px;
     border-radius: 4px;
-    background: rgba($background, 0.95);
+    background: rgba($background,1);
+    box-shadow: 1px 2px 15px rgba(0,0,0,0.1);
     &-top {
         top: 20%;
     }
     &-bottom {
-        bottom: 20%;
+        bottom: 12%;
     }
     > header {
         padding: $gutter/2 $gutter 0 $gutter;
         overflow: hidden;
         font-size: $big;
     }
-    > main {
-        font-size: $normal;
+    &__body {
         padding: $gutter;
         overflow-x: hidden;
         overflow-y: auto;
         min-height: 2rem;
+        max-height: 61.8vh;
     }
 }
 
