95ef0157bfa41ec2b1f3bea7e37164ede0e6643c
宁宁 <383514580@qq.com>
2018-01-17 20:32:31 +0800

    :whale:  取消边线

--- src/packages/Collapse/CollapseItem.vue
+++ src/packages/Collapse/CollapseItem.vue
@@ -12,126 +12,125 @@
     </div>
 </template>
 <script>
-import { getHeight } from '../../utils/dom';
-export default {
-    name: 'AtomCollapseItem',
+    import {
+        getHeight
+    } from '../../utils/dom';
+    export default {
+        name: 'AtomCollapseItem',
 
-    props: {
-        isOpen: {
-            type: Boolean,
-            default: false
-        },
+        props: {
+            isOpen: {
+                type: Boolean,
+                default: false
+            },
 
-        hasLine: {
-            type: Boolean,
-            default: false
-        },
+            hasLine: {
+                type: Boolean,
+                default: false
+            },
 
-        hasAnimate: {
-            type: Boolean,
-            default: true
-        }
-    },
-
-    data() {
-        return {
-            index: 0,
-            isUnfolded: false
-        };
-    },
-
-    mounted() {
-        if (this.$isServer) return;
-        this.index = this.$parent.count;
-        this.$parent.count++;
-        this.$parent.status.push(this.isOpen);
-    },
-
-    methods: {
-        toggle() {
-            this.isUnfolded = !this.isUnfolded;
-            if (this.$parent.isAccordion) {
-                this.$parent.status = this.$parent.status.map((isOpen, i) => {
-                    return this.index == i ? this.isUnfolded : false;
-                });
-            } else {
-                this.$parent.status = this.$parent.status.map((isOpen, i) => {
-                    return this.index == i ? this.isUnfolded : isOpen;
-                });
-            }
-            this.$emit('update:isOpen', this.isUnfolded);
-            if (this.isUnfolded) {
-                this.$emit('change', this.index);
-            }
-        }
-    },
-
-    watch: {
-        isOpen: {
-            immediate: true,
-            handler(value) {
-                this.isUnfolded = value;
+            hasAnimate: {
+                type: Boolean,
+                default: true
             }
         },
 
-        ['$parent.status'](value) {
-            for (let k in value) {
-                if (this.index == k) {
-                    this.isUnfolded = value[k];
-                    break;
+        data() {
+            return {
+                index: 0,
+                isUnfolded: false
+            };
+        },
+
+        mounted() {
+            if (this.$isServer) return;
+            this.index = this.$parent.count;
+            this.$parent.count++;
+            this.$parent.status.push(this.isOpen);
+        },
+
+        methods: {
+            toggle() {
+                this.isUnfolded = !this.isUnfolded;
+                if (this.$parent.isAccordion) {
+                    this.$parent.status = this.$parent.status.map((isOpen, i) => {
+                        return this.index == i ? this.isUnfolded : false;
+                    });
+                } else {
+                    this.$parent.status = this.$parent.status.map((isOpen, i) => {
+                        return this.index == i ? this.isUnfolded : isOpen;
+                    });
+                }
+                this.$emit('update:isOpen', this.isUnfolded);
+                if (this.isUnfolded) {
+                    this.$emit('change', this.index);
+                }
+            }
+        },
+
+        watch: {
+            isOpen: {
+                immediate: true,
+                handler(value) {
+                    this.isUnfolded = value;
+                }
+            },
+
+            ['$parent.status'](value) {
+                for (let k in value) {
+                    if (this.index == k) {
+                        this.isUnfolded = value[k];
+                        break;
+                    }
                 }
             }
         }
-    }
-};
+    };
 </script>
 <style scoped lang="scss">
-@import '../../scss/variables.scss';
-.atom-collapse__item {
-    position: relative;
-    border-bottom: 1px solid $lighter;
-
-    > .item__header {
-        display: flex;
-        padding: $gutter;
-        height: 24px;
-        line-height: 24px;
-        box-sizing: content-box;
-        font-size: $big;
-        &--line {
-            border-bottom: 1px solid $lightest;
-        }
-        .header__arrow {
-            display: inline-block;
-            background: url('../../assets/more_unfold.svg');
-            background-size: 100%;
-            width: 24px;
+    @import '../../scss/variables.scss';
+    .atom-collapse__item {
+        position: relative;
+        >.item__header {
+            display: flex;
+            padding: $gutter;
             height: 24px;
-            margin-right: $gutter;
-            transition: transform $duration;
-            will-change: transform;
-            &--open {
-                transform: rotate(0);
+            line-height: 24px;
+            box-sizing: content-box;
+            font-size: $big;
+            &--line {
+                border-bottom: 1px solid $lightest;
             }
-            &--close {
-                transform: rotate(-90deg);
+            .header__arrow {
+                display: inline-block;
+                align-self: center;
+                background: url('../../assets/more_unfold.svg');
+                background-size: 100%;
+                width: 20px;
+                height: 20px;
+                margin-right: $gutter;
+                transition: transform $duration;
+                will-change: transform;
+                &--open {
+                    transform: rotate(0);
+                }
+                &--close {
+                    transform: rotate(-90deg);
+                }
+            }
+        }
+        >.item__body {
+            padding: 0 $gutter $gutter $gutter;
+            overflow: hidden;
+            line-height: 1.5;
+        }
+        .item__header--line+.item__body {
+            padding: $gutter;
+        }
+        &:not([is-unfolded]) {
+            .item__header--line {
+                border-bottom: 0 none;
             }
         }
     }
-
-    > .item__body {
-        padding: 0 $gutter $gutter $gutter;
-        overflow: hidden;
-        line-height: 1.5;
-    }
-    .item__header--line + .item__body {
-        padding: $gutter;
-    }
-
-    &:not([is-unfolded]) {
-        .item__header--line {
-            border-bottom: 0 none;
-        }
-    }
-}
 </style>
