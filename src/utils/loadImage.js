04cae3394730093b231a6948ec5f1778d57cbbd9
宁宁 <383514580@qq.com>
2018-01-18 15:24:35 +0800

    :whale: 返回宽高

--- src/utils/loadImage.js
+++ src/utils/loadImage.js
@@ -5,18 +5,24 @@ export default function (src, {isCrossOrigin = false, onInit = () => { }, onSucc
     if(isCrossOrigin) img.crossOrigin = 'anonymous';
     onInit(img);
     img.onload = event => {
+        event.width = img.width;
+        event.height = img.height;
         event.costTime = Date.now() - startTime;
         onSuccess(event);
         img.null;
     };
 
     img.onerror = event => {
+        event.width = img.width;
+        event.height = img.height;
         event.costTime = Date.now() - startTime;
         onError(event);
         img.null;
     };
 
     img.onabort = event => {
+        event.width = img.width;
+        event.height = img.height;   
         event.costTime = Date.now() - startTime;
         onAbort(event);
         img.null;
