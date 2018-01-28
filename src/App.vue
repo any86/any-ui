c6ae6066f3a8b1a9afbb13913584d2bfca1dd604
宁宁 <383514580@qq.com>
2018-01-28 21:56:02 +0800

    :whale: 备份

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
         </ul>
 
         <v-app-bar @click-arrow="$router.back()" :has-arrow="'index' !== $route.name"><a @click="$router.push({'path': '/'})">Atom-UI</a></v-app-bar>
-<div class="border-bottom-none border-danger">123</div>
+<div class="triangle-success"></div>
         
         <main class="app-main">
             <transition name="zoom" mode="out-in">
