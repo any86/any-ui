23aff9242bea5d5106bd594b3f012fb3594ac3dc
宁宁 <383514580@qq.com>
2018-01-30 08:50:42 +0800

    :whale: 新建增加icon

---
+++ src/packages/Icon/Add.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg v-on="$listeners" v-bind="$attrs" :viewBox="`0 0 40 40`" :style="{width: `${size}px`, height: `${size}px`}" class="atom-icon-add" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <line x1="5" y1="20" x2="35" y2="20" :stroke-width="2" />
+        <line x1="20" y1="5" x2="20" :y2="35" :stroke-width="2"/>
+    </svg>
+</template>
+<script>
+export default {
+    name: 'AtomIconAdd',
+
+    props: {
+        size: {
+            default: 48
+        }
+    }
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/variables.scss';
+line {
+    stroke: $light;
+}
+</style>
