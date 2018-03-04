4f13378e9b2ab6f9788992923461fd8458299dab
宁宁 <383514580@qq.com>
2018-03-04 21:16:59 +0800

    :zap: [build] 1.0.109

--- build/publish.js
+++ build/publish.js
@@ -1,5 +1,6 @@
 'use strict'
 require('./check-versions')()
+const ci = require('../ci.js');
 
 process.env.NODE_ENV = 'production'
 
@@ -32,9 +33,6 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
+    ci();
   })
 })
