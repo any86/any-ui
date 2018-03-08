692c28d739f50d33ba28775ad55abb5ffbd05e07
宁宁 <383514580@qq.com>
2018-03-08 17:03:24 +0800

    :whale: 暴露更多参数, 文案变得可调

--- src/pages/AlertDemo.vue
+++ src/pages/AlertDemo.vue
@@ -1,13 +1,37 @@
 <template>
-    <a-scroll-view class="flex">
-        <a-button type="primary" :is-radius="true" :is-ghost="true" @click="$alert('hello atom !')" class="gutter flex-item--center">
-            Alert
+    <a-scroll-view class="fill">
+        <a-button type="primary" :is-radius="true" :is-block="true" :is-ghost="true" @click="ok1" class="gutter-top">
+            默认无标题
+        </a-button>
+
+        <a-button type="primary" :is-radius="true" :is-block="true" :is-ghost="true" @click="ok2" class="gutter-top">
+            自定义: title | okText
+        </a-button>
+
+
+        <a-button type="primary" :is-radius="true" :is-block="true" :is-ghost="true" @click="ok3" class="gutter-top">
+            上方显示: align: 'top'
         </a-button>
     </a-scroll-view>
 </template>
 <script>
 export default {
-    name: 'AlertDemo'
+    name: 'AlertDemo',
+
+    methods: {
+        ok1(){
+            this.$alert('你好, 我是atom !');
+        },
+
+        ok2(){
+            this.$alert('你好, 我是atom !', {title: '我是标题!', okText: '自定义确定'})
+        },
+
+        ok3(){
+            this.$alert('我在上方显示!', {align: 'top'})
+            
+        }
+    }
 };
 </script>
 <style scoped lang="scss">
