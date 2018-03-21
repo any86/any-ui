808c4e5aa800dd782df6101f1a487f2bc87f460d
宁宁 <383514580@qq.com>
2018-03-21 11:38:22 +0800

    :zap: [build] 1.0.139

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var rename = require("gulp-rename");
+ 
+gulp.task('css', function() {
+    return gulp.src('./src/scss/global.scss')
+        .pipe(sass.sync())
+        .pipe(autoprefixer({
+            browsers: ['ie > 9', 'last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename('atom.ui.css'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css', function() {
+    return gulp.src('./src/scss/global.scss')
+        .pipe(sass.sync())
+        .pipe(autoprefixer({
+            browsers: ['ie > 9', 'last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename('atom.ui.css'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(cssmin())
+        .pipe(rename('atom.ui.min.css'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copyfont', function() {
+    return gulp.src('./src/fonts/**')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('build', ['css']);
