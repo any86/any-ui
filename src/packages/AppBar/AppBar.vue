b76870bf888f795a681303ddffb002d901325b95
DESKTOP-L4DMSPQ\Ning <383514580@qq.com>
2018-04-05 16:22:12 +0800

    回复按需加载前版本

---
+++ src/packages/AppBar/AppBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <header class="atom-app-bar">
+        <span class="bar__left">
+            <a-icon 
+                v-ripple="true" 
+                :size="24" 
+                value="arrow" 
+                style="transform: rotate(180deg)"
+                @click="clickArrow" />
+            <slot name="left"></slot>
+        </span>
+        <span class="bar__center">
+            <slot></slot>
+        </span>
+        <span class="bar__right">
+            <slot name="right"></slot>
+        </span>
+    </header>
+</template>
+<script>
+import AIcon from '../Icon';
+export default {
+    name: 'AtomAppBar',
+
+    props: {
+        isRouterBack: {
+            type: Boolean,
+            default: true
+        }
+    },
+
+    methods: {
+        clickArrow() {
+            if (this.isRouterBack) {
+                this.$router.back();
+            } else {
+                this.$emit('click-arrow');
+            }
+        }
+    },
+
+    components: { AIcon }
+};
+</script>
---
+++ src/packages/QRCode/QRCode.vue
@@ -0,0 +1,60 @@
+<template>
+    <!-- canvas默认是宽度300, 高度300, 为了避免渲染初期影响父元素的尺寸, 所以初期限制为0 -->
+    <canvas width="0" height="0" class="atom-qrcode" />
+</template>
+<script>
+import QRCode from 'qrcode';
+
+export default {
+    name: 'AtomQRCode',
+
+    props: {
+        value: {
+            default() {
+                return this.$isServer ? '' : window.location.href;
+            }
+        }
+    },
+
+    mounted() {
+        if (!this.$isServer) {
+            this.renderQRCode(this.value);
+        }
+    },
+
+    methods: {
+        renderQRCode(string) {
+            const str = String(string);
+            QRCode.toCanvas(
+                this.$el,
+                str,
+                // {
+                //     color: {
+                //         dark: '#000', // Blue dots
+                //         light: '#fff' // Transparent background
+                //     }
+                // },
+                error => {
+                    if (error) console.error(error);
+                }
+            );
+        }
+    },
+
+    watch: {
+        value: {
+            // immediate: true, // 这个阶段还没有$el
+            handler(value) {
+                this.renderQRCode(value);
+            }
+        }
+    }
+};
+</script>
+<style scoped lang="scss">
+@import '../../scss/variables.scss';
+.atom-qrcode {
+    width: 2rem;
+    height: 2rem;
+}
+</style>
