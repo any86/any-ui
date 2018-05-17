4cdb77951357a891d62200e5204658957e92198e
宁宁 <383514580@qq.com>
2018-05-17 17:06:50 +0800

    :zap: 重构tap

--- src/utils/touch2/recognitions/Tap.js
+++ src/utils/touch2/recognitions/Tap.js
@@ -19,39 +19,53 @@ export default class TapRecognizer {
      * @param {Object} 手势数据 
      * @param {Boolean} 是否同时识别doubletap, 如果不识别,tap会反应快些(50ms)
      */
-    end(fingerInput, hasDoubletap = false) {
-        this.$fingerInput = fingerInput;
-        // 判断是否[tap|doubeltap]
-        // 点击后250ms以内
-        // 位移小于2px
-        if (250 > this.$fingerInput.offsetTime && 2 > this.$fingerInput.absOffsetX && 2 > this.$fingerInput.absOffsetY) {
-            if (0 === this.tapCount) {
-                // 识别[tap]
-                // 如果没有绑定双击事件, 那么让tap快速执行, 不需要等待200ms去识别doubletap
-                this.tapCount++;
-                this.tapTimeout = setTimeout(() => {
-                    this.type = 'tap';
-                    this.tapCount = 0;
-                }, hasDoubletap ? 200 : 50);
-            } else {
-                // 识别[doubletap]
-                this.cancel();
-                this.type = 'doubletap';
-                this.tapCount = 0;
+    end(fingerInput, recognizeDoubleTap = false) {
+        const {
+            offsetTime,
+            absOffsetX,
+            absOffsetY
+        } = fingerInput;
+
+        return new Promise((resolve, reject) => {
+            // 判断是否[tap|doubletap]
+            // 点击后250ms以内
+            // 位移小于2px
+            if (250 > offsetTime && 2 > absOffsetX && 2 > absOffsetY) {
+                this.$fingerInput = fingerInput;
+                // log(`recognizeDoubleTap: ${recognizeDoubleTap}`)
+                if (recognizeDoubleTap) {
+                    this.tapCount++;
+                    clearTimeout(this.tapTimeout);
+                    if (1 === this.tapCount) {
+                        this.tapTimeout = setTimeout(() => {
+                            this.type = 'tap';
+                            this.tapCount = 0;
+                            resolve(this.type);
+                        }, 200);
+                    } else if (2 === this.tapCount) {
+                        this.type = 'doubletap';
+                        this.tapCount = 0;
+                        resolve(this.type);
+                    }
+                } else {
+                    setTimeout(() => {
+                        this.type = 'tap';
+                        resolve(this.type);
+                    }, 0);
+                }
             }
-            return this.type;
-        }
+        });
     };
 
-    cancel() {
-        clearTimeout(this.tapTimeout);
-        this.tapTimeout = null;
-    };
+    // cancel() {
+
+    // };
 
     computedData() {
         return {
             type: this.type,
-            nativeEvent: this.$fingerInput.nativeEvent
+            nativeEvent: this.$fingerInput.nativeEvent,
+            times: this.tapCount
         }
     }
 };
