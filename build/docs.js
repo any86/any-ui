5c5aa7431c0e16c514b9586df29732b8a32b37e0
宁宁 <383514580@qq.com>
2018-06-30 11:09:36 +0800

    修改文档发布成功文案

--- build/docs.js
+++ build/docs.js
@@ -11,5 +11,5 @@ shell.exec(`mv ${src} ${dest}`);
 ghpages.publish('./docs/dist', {
     branch: 'gh-pages',
 }, function(err) {
-  console.log('docs同步完成!');
+  console.log('docs同步gh-pages完成!');
 });
