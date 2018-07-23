5c7192d27bd45b385330ba6e20993260b431735d
宁宁 <383514580@qq.com>
2018-07-23 16:30:33 +0800

    调整样式

--- src/pages/AlertDemo.vue
+++ src/pages/AlertDemo.vue
@@ -1,16 +1,16 @@
 <template>
     <a-scroll-view class="fill">
-        <a-button type="primary" :is-radius="true" :is-block="true" :is-ghost="true" @click="ok1" class="gutter-top">
+        <a-button type="primary" :is-radius="true" outline @click="ok1" class="gutter-top">
             默认无标题
         </a-button>
 
-        <a-button type="primary" :is-radius="true" :is-block="true" :is-ghost="true" @click="ok2" class="gutter-top">
-            自定义: title | okText
+        <a-button type="primary" :is-radius="true" outline @click="ok2" class="gutter-top">
+            自定义: title | okText | 底部对齐
         </a-button>
 
 
-        <a-button type="primary" :is-radius="true" :is-block="true" :is-ghost="true" @click="ok3" class="gutter-top">
-            上方显示: align: 'top'
+        <a-button type="primary" :is-radius="true" outline @click="ok3" class="gutter-top">
+            顶部对齐: align: 'top'
         </a-button>
     </a-scroll-view>
 </template>
@@ -24,7 +24,7 @@ export default {
         },
 
         ok2(){
-            this.$alert('你好, 我是atom !', {title: '我是标题!', okText: '自定义确定'})
+            this.$alert('你好, 我是atom !', {title: '我是标题!', okText: '自定义确定', align:'bottom'})
         },
 
         ok3(){
