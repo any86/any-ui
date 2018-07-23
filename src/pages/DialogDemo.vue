7fecb9aa3994f7abd49b0f2f639cafa9db6cdd7c
宁宁 <383514580@qq.com>
2018-07-23 10:33:49 +0800

    dialog增加align=center, 实现垂直居中

--- src/pages/DialogDemo.vue
+++ src/pages/DialogDemo.vue
@@ -1,48 +1,35 @@
 <template>
-    <v-scroll-view class="fill flex">
-        <v-dialog :is-show.sync="isShow">
-            <v-cell>正常</v-cell>
-            <v-cell>困难</v-cell>
-            <v-cell>地狱</v-cell>
+    <main class="fill flex">
+        <a-dialog align="center" :is-show.sync="isShow">
+            <a-cell>正常</a-cell>
+            <a-cell>困难</a-cell>
+            <a-cell>地狱</a-cell>
 
-            <v-button type="primary"  class="gutter-top">开始游戏</v-button>
-            <v-button outline  @click="isShow=false" class="gutter-top">离开</v-button>
-        </v-dialog>
+            <a-button type="primary"  class="gutter-top">开始游戏</a-button>
+            <a-button outline  @click="isShow=false" class="gutter-top">离开</a-button>
+        </a-dialog>
 
-        <v-button outline  type="primary" @click="handle('bottom')" class="gutter-top flex-item--center">
+        <a-button outline  type="primary" @click="handle('bottom')" class="gutter-top flex-item--center">
             打开dialog
-        </v-button>
-
-    </v-scroll-view>
+        </a-button>
+    </main>
 </template>
 <script>
-import VDialog from '@/packages/Dialog/Dialog';
-import VCell from '@/packages/Cell/Cell';
-import VButton from '@/packages/Button/Button';
-import VScrollView from '@/packages/ScrollView/ScrollView';
 export default {
     name: 'DialogDemo',
 
     data() {
         return {
-            isShow: false
+            isShow: false,
         };
     },
 
     methods: {
         handle(from) {
             this.isShow = true;
-        }
+        },
     },
-
-    components: {
-        VButton,
-        VDialog,
-        VCell,
-        VScrollView
-    }
 };
 </script>
 <style scoped lang="scss">
-
 </style>
