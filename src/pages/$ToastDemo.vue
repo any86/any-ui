04f36353b01d5259854182c8ceb165cab4577c58
宁宁 <383514580@qq.com>
2018-07-31 09:08:34 +0800

    增加最小高度

--- src/pages/$ToastDemo.vue
+++ src/pages/$ToastDemo.vue
@@ -1,12 +1,12 @@
 <template>
     <a-scroll-view class="demo-page">
-        <a-cell @click="$toast('3秒后关闭')">默认样式, 3秒后自动关闭</a-cell>
-        <a-cell @click="$toast('3秒后关闭', {type:'success'})"><a class="text-success">成功</a>样式, 3秒后自动关闭</a-cell>
-        <a-cell @click="$toast('3秒后关闭', {type:'warning'})"><a class="text-warning">警告</a>样式, 3秒后自动关闭</a-cell>
-        <a-cell @click="$toast('3秒后关闭', {type:'danger'})"><a class="text-danger">错误</a>样式, 3秒后自动关闭</a-cell>
-        <a-cell @click="$toast('3秒后关闭', {type:'loading', time:0})">加载中样式, 3秒后自动关闭</a-cell>
+        <a-cell @click="$toast('2秒后关闭')">默认样式, 2秒后自动关闭</a-cell>
+        <a-cell @click="$toast('2秒后关闭', {type:'success'})"><a class="text-success">成功</a>样式, 2秒后自动关闭</a-cell>
+        <a-cell @click="$toast('2秒后关闭', {type:'warning'})"><a class="text-warning">警告</a>样式, 2秒后自动关闭</a-cell>
+        <a-cell @click="$toast('2秒后关闭', {type:'danger'})"><a class="text-danger">错误</a>样式, 2秒后自动关闭</a-cell>
+        <a-cell @click="$toast('2秒后关闭', {type:'loading'})">加载中样式, 2秒后自动关闭</a-cell>
         <section class="fill">
-            <a-button @click="test">测试</a-button>
+            <a-button @click="test">手动关闭</a-button>
         </section>
     </a-scroll-view>
 </template>
@@ -22,7 +22,7 @@ export default {
 
     methods: {
         test() {
-            this.$toast('开始test', {time:-1});
+            this.$toast('开始test', { time: -1 });
             setTimeout(() => {
                 this.$toast.close();
             }, 1000);
