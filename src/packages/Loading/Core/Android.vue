ea6fefcd08d4bffbe1d4935c5ea8f10a163f7624
宁宁 <383514580@qq.com>
2018-08-02 11:45:04 +0800

    优化定位

--- src/packages/Loading/Core/Android.vue
+++ src/packages/Loading/Core/Android.vue
@@ -1,7 +1,7 @@
 <template functional>
-    <svg width="36" height="36" viewBox="25 25 50 50" 
+    <svg width="36" height="36" viewBox="0 0 50 50" 
     :class="['atom-loading-android', '' !== props.type && `text-${props.type}`]">
-        <circle cx="50" cy="50" r="20" fill="none" stroke="currentColor" stroke-width="5" stroke-miterlimit="10"></circle>
+        <circle cx="25" cy="25" r="20" fill="none" stroke="currentColor" stroke-width="5" stroke-miterlimit="10"></circle>
     </svg>
 </template>
 <script>
