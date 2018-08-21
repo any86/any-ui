46a8d270c68172cfd88ca7c9cf3901397caadca4
宁宁 <383514580@qq.com>
2018-08-21 11:00:01 +0800

    删除坏图

--- src/pages/CarouselDemo.vue
+++ src/pages/CarouselDemo.vue
@@ -9,7 +9,7 @@
             </v-carousel>
         </v-group>
 
-        <!-- <v-group title="每屏多图 | 循环 | 自动播放">
+        <v-group title="每屏多图 | 循环 | 自动播放">
             <v-carousel  v-model="activeIndex2" :is-loop="true" :is-auto-play="true" :slides-per-view="2.2" :delay="1000">
                 <v-carousel-item v-for="image in shortImages" :key="image">
                     <img :lazy-src="image" width="100%" height="200" />
@@ -18,14 +18,14 @@
         </v-group>
 
         <v-group title="循环 | 自动播放 | 外部控制 | 不显示默认分页按钮">
-            <v-carousel @init="carouselInit" v-model="activeIndex3" :real-index.sync="realIndex3" :is-auto-play="true" :is-loop="true" :has-page-btn="false">
+            <v-carousel @init="carouselInit" v-model="activeIndex3" :slides-per-view="1.1" :real-index.sync="realIndex3" :is-auto-play="false" :is-loop="false" :has-page-btn="false">
                 <v-carousel-item v-for="image in longImages" :key="image">
                     <img :lazy-src="image" width="100%" height="200" />
                 </v-carousel-item>
             </v-carousel>
-        </v-group> -->
+        </v-group>
 
-        <v-button v-for="n in pageBtnCount" :key="n" type="primary" size="sm"  @click="activeIndex3=n-1" class="gutter-sm" :class="{'active': realIndex3+1 ==n}">{{n-1}}</v-button>
+        <v-button v-for="n in pageBtnCount" :key="n" :type="realIndex3+1 ==n ? 'danger' :'dark'" inline  @click="activeIndex3=n-1" class="gutter-sm">{{n-1}}</v-button>
 
     </v-scroll-view>
 </template>
@@ -42,7 +42,7 @@ export default {
     name: 'CarouselDemo',
 
     data() {
-        const shortImages = ['https://static.soufeel.com/media/catalog/product/cache/0/thumbnail/280x280/9df78eab33525d08d6e5fb8d27136e95/C/R/CR05_1.png', 'https://static.soufeel.com/media/catalog/product/cache/0/thumbnail/280x280/9df78eab33525d08d6e5fb8d27136e95/C/R/CR06_3.png', 'https://static.soufeel.com/media/catalog/product/cache/0/thumbnail/280x280/9df78eab33525d08d6e5fb8d27136e95/C/R/CR09_1.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/R/2/R215_2.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/R/CR17_3.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/R/CR20.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL374.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL401.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL219.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL433.png'];
+        const shortImages = ['https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/R/2/R215_2.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL374.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL401.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL219.png', 'https://static.soufeel.com/media/catalog/product/cache/0/small_image/280x280/9df78eab33525d08d6e5fb8d27136e95/C/X/CXL433.png'];
 
         const longImages = ['https://p2.imaiyuan.com/media/catalog/product/D/P/DP062.png','http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/11/43/1143dc058ee817f1c08074ced118b5cf.jpg', 'http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/b2/f9/b2f9453ff2b87e81867e003ea66551ea.png', 'https://dn-geekpark-new.qbox.me/uploads/image/file/65/36/653617a7ab15d06e1630389e7e5e058a.jpg', 'https://dn-geekpark-new.qbox.me/uploads/image/file/2d/cc/2dccb0fb30d695295c2566f43aa82fcd.jpg', 'http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/97/cb/97cb0602bdf36aff4dd0586fd4f1ab41.jpg', 'http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/50/6a/506a2477e321039984b438cce6961c6d.jpg', 'http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/4f/d7/4fd7010e128fdf69e4c3edcd30aa1f00.jpeg', 'https://dn-geekpark-new.qbox.me/uploads/image/file/44/ed/44ed15cfada71a4063411cf20af31877.jpg', 'http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/e0/b8/e0b850d7e31d4ee7ef1b829eda0423cf.JPG', 'http://ovsnhdoi9.bkt.clouddn.com/uploads/image/file/d6/aa/d6aae7975bdb38da5c5982681b84ffd4.jpeg'];
         return {
@@ -82,7 +82,4 @@ export default {
 </script>
 <style scope lang="scss">
 @import '../scss/variables.scss';
-.active {
-    background: $danger !important;
-}
 </style>
