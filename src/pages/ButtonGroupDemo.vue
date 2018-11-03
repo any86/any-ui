b630d5006deb131db18cf64078f176ad4b072b41
宁宁 <383514580@qq.com>
2018-11-03 09:51:58 +0800

    buttonGroup增加column属性, 支持列布局

--- src/pages/ButtonGroupDemo.vue
+++ src/pages/ButtonGroupDemo.vue
@@ -1,8 +1,16 @@
 <template>
     <a-scroll-view class="fill-sm">
         <a-button-group>
-            <a-button  type="white">white</a-button>
-            <a-button  type="primary">primary</a-button>
+            <a-button type="primary">primary</a-button>
+            <a-button type="white">white</a-button>
+            <a-button type="info">info</a-button>
+        </a-button-group>
+        
+        
+        <a-button-group class="gutter-top" column>
+            <a-button type="white">white</a-button>
+            <a-button type="white">white</a-button>
+            <a-button type="white">white</a-button>
         </a-button-group>
     </a-scroll-view>
 </template>
