ac79219b93d276ead8ebe497ca525e135bd062f0
383514580 <383514580@qq.com>
2019-08-04 19:36:24 +0800

    fix: 修改发布脚本在windows下不能执行的问题

--- build/docs.js
+++ build/docs.js
@@ -2,10 +2,12 @@ var ghpages = require('gh-pages');
 const shell = require('shelljs');
 
 // 拷贝demo到docs/demo
-let src = 'dist/* ';
-let dest = 'docs/dist/demo';
-shell.exec(`mkdir ${dest}`);
-shell.exec(`mv ${src} ${dest}`);
+let src = './dist/* ';
+let dest = './docs/dist/demo';
+// shell.exec(`mv ${src} ${dest}`);
+
+shell.mkdir(dest);
+shell.mv(src, dest);
 
 // 发布
 ghpages.publish('./docs/dist', {
